d0f7d457a4c8c482f35901e9a002982280f89862
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-01-22 12:14:37 +0200

    Commit 2024-01-22 12:14

---
+++ Libradtran/Create_jobs.R
@@ -0,0 +1,80 @@
+#!/usr/bin/env Rscript
+# /* Copyright (C) 2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+#' ---
+#' title:  "......."
+#' author: "Natsis Athanasios"
+#' institute: "AUTH"
+#' affiliation: "Laboratory of Atmospheric Physics"
+#' abstract: "........."
+#' output:
+#'   html_document:
+#'     toc: true
+#'     fig_width:  9
+#'     fig_height: 4
+#'   pdf_document:
+#' date: "`r format(Sys.time(), '%F')`"
+#' ---
+
+
+#+ echo=F, include=T
+rm(list = (ls()[ls() != ""]))
+Script.Name <- "~/MANUSCRIPTS/02_enhancement/Libradtran/Create_jobs.R"
+dir.create("./runtime/", showWarnings = FALSE)
+d <- filelock::lock(paste0("./runtime/", basename(sub("\\.R$",".lock", Script.Name))), timeout = 0)
+Sys.setenv(TZ = "UTC")
+## standard output
+if (!interactive()) {
+    pdf( file = paste0("./runtime/",  basename(sub("\\.R$",".pdf", Script.Name))))
+    sink(file = paste0("./runtime/",  basename(sub("\\.R$",".out", Script.Name))), split = TRUE)
+}
+## error notification function
+options(error = function() {
+    if (interactive()) {
+        system("mplayer /usr/share/sounds/freedesktop/stereo/dialog-warning.oga", ignore.stdout = T, ignore.stderr = T)
+        system(paste("notify-send -u normal -t 30000 ", Script.Name, " 'An error occurred!'"))
+    }
+})
+tic <- Sys.time()
+
+
+
+## __ Document options ---------------------------------------------------------
+knitr::opts_chunk$set(comment    = ""       )
+# knitr::opts_chunk$set(dev        = c("pdf", "png"))
+knitr::opts_chunk$set(dev        = "png"    )
+knitr::opts_chunk$set(out.width  = "100%"   )
+knitr::opts_chunk$set(fig.align  = "center" )
+knitr::opts_chunk$set(cache      =  FALSE   )  ## !! breaks calculations
+knitr::opts_chunk$set(fig.pos    = '!h'     )
+
+## __  Set environment ---------------------------------------------------------
+require(data.table, quietly = TRUE, warn.conflicts = FALSE)
+
+
+
+AER <- fread("Thessaloniki_overall.txt", skip = 6, fill = TRUE)
+# read.csv("Thessaloniki_overall.txt", skip = 6)
+
+
+AER$Month
+
+
+
+
+month.name
+month.abb
+tolower(AER$Month)
+
+
+
+
+
+#' **END**
+#+ include=T, echo=F
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n", Sys.time(), Sys.info()["login"],
+            Sys.info()["nodename"], basename(Script.Name), difftime(tac,tic,units = "mins")))
+if (difftime(tac, tic, units = "sec") > 30) {
+    system("mplayer /usr/share/sounds/freedesktop/stereo/dialog-warning.oga", ignore.stdout = T, ignore.stderr = T)
+    system(paste("notify-send -u normal -t 30000 ", Script.Name, " 'FINISHED'"))
+}
