0bd869d41a4ccd4af3101f603402fffa9dfb0225
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-01-23 14:17:33 +0200

    Commit 2024-01-23 14:17

--- Libradtran/parse_out.R
+++ Libradtran/parse_out.R
@@ -1,12 +1,40 @@
 #!/usr/bin/env Rscript
-# /* Copyright (C) 2019 Athanasios Natsis <natsisthanasis@gmail.com> */
+# /* Copyright (C) 2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+#' ---
+#' title:  "......."
+#' author: "Natsis Athanasios"
+#' institute: "AUTH"
+#' affiliation: "Laboratory of Atmospheric Physics"
+#' abstract: "........."
+#' output:
+#'   html_document:
+#'     toc: true
+#'     fig_width:  9
+#'     fig_height: 4
+#'   pdf_document:
+#' date: "`r format(Sys.time(), '%F')`"
+#' ---
 
-####    Set environment    #####################################################
-closeAllConnections()
+
+#+ echo=F, include=T
 rm(list = (ls()[ls() != ""]))
+Script.Name <- "~/MANUSCRIPTS/02_enhancement/Libradtran/parse_out.R"
+dir.create("./runtime/", showWarnings = FALSE)
+d <- filelock::lock(paste0("./runtime/", basename(sub("\\.R$",".lock", Script.Name))), timeout = 0)
 Sys.setenv(TZ = "UTC")
-tic = Sys.time()
-Script.Name = c("parse_out.R")
+## standard output
+if (!interactive()) {
+    pdf( file = paste0("./runtime/",  basename(sub("\\.R$",".pdf", Script.Name))))
+    sink(file = paste0("./runtime/",  basename(sub("\\.R$",".out", Script.Name))), split = TRUE)
+}
+## error notification function
+options(error = function() {
+    if (interactive()) {
+        system("mplayer /usr/share/sounds/freedesktop/stereo/dialog-warning.oga", ignore.stdout = T, ignore.stderr = T)
+        system(paste("notify-send -u normal -t 30000 ", Script.Name, " 'An error occurred!'"))
+    }
+})
+tic <- Sys.time()
 
 library(data.table)
 source("~/FUNCTIONS/R/data.R")
@@ -103,3 +131,12 @@ saveRDS(storage, model_cs)
 file.remove(out_files)
 file.remove(err_files)
 
+#' **END**
+#+ include=T, echo=F
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n", Sys.time(), Sys.info()["login"],
+            Sys.info()["nodename"], basename(Script.Name), difftime(tac,tic,units = "mins")))
+if (difftime(tac, tic, units = "sec") > 30) {
+    system("mplayer /usr/share/sounds/freedesktop/stereo/dialog-warning.oga", ignore.stdout = T, ignore.stderr = T)
+    system(paste("notify-send -u normal -t 30000 ", Script.Name, " 'FINISHED'"))
+}
