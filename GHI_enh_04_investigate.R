2b29ceb2dfb9b80bdc0477690023d2b9c18d115d
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-07 13:12:07 +0300

    Commit 2024-10-07 13:12

--- GHI_enh_04_investigate.R
+++ GHI_enh_04_investigate.R
@@ -1865,15 +1865,6 @@ p2
 
 
 
-
-
-
-
-
-
-
-
-
 ## Yearly trends with data completeness  ---------------------------------------
 
 #' \newpage
@@ -1915,8 +1906,8 @@ yeartrends <- rbind(yeartrends,
 grob <- grobTree(
   textGrob(
     label = TeX(
-      paste("Trend:  $", format(round(lmY1$coefficients[2], 1), nsmall = 1),
-            " \\pm",      format(round(2 * d2[2,2],          1), nsmall = 1), ## show 2 sigma
+      paste("Trend:  $", paste(format(round(lmY1$coefficients[2], 1), nsmall = 1)),
+            "\\pm",      paste(format(round(2 * d2[2,2],          1), nsmall = 1)), ## show 2 sigma
             "\\,kJ/m^2/year$")),
     x = 0.95,  y = 0.05, hjust = 1,
     gp = gpar(col = "black", fontsize = 13, fontface = "bold")
